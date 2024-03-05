31ee5187aab6ec3ca66f7ffddad287b15db32323
wendymackennah <wendymackennah5@gmail.com>
2024-03-05 12:54:46 +0300

    done assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,23 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    
+    def __init__ (self,name="fido", breed=""):
+        self.name = name
+        self.breed=breed
+
+
+    def  get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if(type(name) == str  and 0 < len(name) <=25 ) :
+            print("Breed must be in list of approved breeds.")
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+        
+
+
+    name = property(get_name,set_name)
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,23 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__ (self,name="Guido", job="Sales"):
+        self.name = name
+        self.job = job
+
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name.title() if hasattr(self, '_name') else ''
+
+    
+    def set_name(self, name):
+        if(type(name) == str  and 0 < len(name) <=25 ) :
+            print("Job must be in list of approved jobs.")
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+        
+
+
+    name = property(get_name,set_name)
